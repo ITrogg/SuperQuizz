472477a6be5cdccda8d70d8278b463d5a846eaef
Imanol Deleporte <mana.gyllenhaalic@gmail.com>
2024-04-12 13:32:18 +0200

    Structure du fichier js pour prévenir les conflits

---
+++ main.js
@@ -0,0 +1,47 @@
+//  Tableau questions 
+const questions = [
+  { question : "Comment ça va ?",
+    goodanswer : "niquel",
+    wronganswer1 : "je sais pas",
+    wronganswer2 : "pas ouf",
+    wronganswer3 : "je te le dirais pas"
+  },
+  {question: "bonjour",
+  goodanswer : "Javascript",
+  wronganswer1 : "HTML",
+  wronganswer2 : "CSS",
+  wronganswer3 : "Python"
+  },
+  {question: "Quelles sont les meilleures pâtes ?",
+  goodanswer : "Penne",
+  wronganswer1 : "Macaroni",
+  wronganswer2 : "Nouilles",
+  wronganswer3 : "Fusilli"
+  }
+]
+
+/* Fonction de démarrage  */
+    // Au clique Lire le prénom 
+
+/* Fonction d'affichage d'une question */ 
+    // doit afficher les réponses dans un ordre aléatoire
+
+
+/* Fonction de vérification de la réponse du joueur */
+      // lire la réponse
+      //retourner vrai ou faux
+
+
+/* Fonction fin de temps de réponse */
+      // là je sais vraiment pas 
+
+
+/* Fonction d'affichage fin de quizz */
+      // Affiche le score 
+      // Affiche un message personnalisé avec le prénom en fonction du score obtenu 
+
+
+/* Boucle */
+//  affiche question 
+//  vérifie réponse
+//  incrémente score
